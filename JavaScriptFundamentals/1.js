1996576208fef4a1ecf25ce53cc9ee2c7e59f7db
mpk02 <martinkuberski@gmail.com>
2022-06-25 18:34:19 +0100

    Solve Fundamentals Part 1 exercises

---
+++ JavaScriptFundamentals/1.js
@@ -0,0 +1,28 @@
+//exercises 1,2,3
+console.log(12 + 13);
+console.log(1 + 3 + 5 + 7 + 9 + 11);
+console.log( (4 + 6 + 9) / 77);
+//exercise 4
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+//exercise 5
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
+//other
+let numA = 625;
+let numB = 25;
+let numC = 6;
+let textB = "6";
+console.log(textB === numC);
+console.log(textB == numC);
+console.log(numA);
+numB = textB + numB;
+console.log(numB);
+console.log(numA === numB);
+console.log(numA == numB);
+console.log(numA === Number(numB));
